6fc366ec836b452b5d946e93a9a2c88542f0cb40
Ishaan Radia <iiradia@ncsu.edu>
2020-03-19 18:57:26 -0400

    First Java LeetCode

---
+++ Java/Easy/344Reverse String.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+      char temp = ' ';
+        for (int i = 0; i < s.length; i++){
+          
+          if (i < (s.length-i-1))
+          {
+          	temp = s[i];
+          	s[i] = s[s.length - i - 1];
+         	s[s.length - i - 1] = temp;
+          }
+        }
+    }
+}
