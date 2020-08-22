4612c06117eb58115e30ca85f668af8f4b1399b2
Ishaan Radia <iiradia@ncsu.edu>
2020-08-21 23:24:53 -0400

    Added alt solution to sort array by parity.

--- Python/Easy/905Sort Array by Parity.py
+++ Python/Easy/905Sort Array by Parity.py
@@ -5,3 +5,18 @@ class Solution(object):
         :rtype: List[int]
         """
         return([i for i in A if i%2==0]+[i for i in A if i%2!=0])
+
+    
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        
+        output = []
+        for i in range(len(A)):
+            if A[i] % 2 == 0:
+                output.append(A[i])
+            
+        for i in range(len(A)):
+            if A[i] % 2 != 0:
+                output.append(A[i])
+            
+        return output
+        
